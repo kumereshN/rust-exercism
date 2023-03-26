6f5bbd08bfaaf2ec87ec299c327c4f1f66a23e65
kumereshN <kumeresh6@hotmail.com.com>
2023-03-26 18:23:15 +0800

    completed short-fibonacci

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,16 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let magazine_map = magazine.iter().fold(HashMap::new(), |mut map, str| {
+        *map.entry(str).or_insert(0) += 1;
+        map
+    });
+
+    let notes_map = note.iter().fold(HashMap::new(), |mut map, str| {
+        *map.entry(str).or_insert(0) += 1;
+        map
+    });
+
+    notes_map.iter()
+             .all(|(word, count)| {magazine_map.get(word).unwrap_or(&0) >= count})
 }
