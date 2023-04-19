d56154367f9f52fb4e32fcf57a1c1391a437218a
kumereshN <kumeresh6@hotmail.com.com>
2023-04-19 14:46:15 +0800

    completed clock exercism

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,29 @@
-pub struct Clock;
+use std::fmt::{Formatter, Display, Result};
+
+const MINUTES_PER_HOUR: i32 = 60;
+const MINUTES_PER_DAY: i32 = 1440;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Clock {
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Self {
+            minutes: (hours * MINUTES_PER_HOUR + minutes).rem_euclid(MINUTES_PER_DAY)
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {minutes} minutes to existing Clock time");
+        Self {
+            minutes: (self.minutes + minutes).rem_euclid(MINUTES_PER_DAY)
+        }
+    }
+}
+
+impl Display for Clock{
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }
