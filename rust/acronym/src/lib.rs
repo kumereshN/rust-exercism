3edb7167deefd02a6c78ed31902630d841faab97
kumereshN <kumeresh6@hotmail.com.com>
2023-04-28 15:48:34 +0800

    completed acronym

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -14,12 +14,12 @@ fn is_all_uppercase(s: &str) -> bool {
     s.chars().all(|c| c.is_uppercase())
 }
 
-fn has_puntuation(s: &str) -> bool {
-    s.chars().any(|c| c.is_alphabetic())
+fn has_punctuation(s: &str) -> bool {
+    s.chars().any(|c| c.is_ascii_punctuation())
 }
 
 pub fn abbreviate(phrase: &str) -> String {
-    let mut accronym = String::new();
+    let mut acronym = String::new();
 
     if phrase.is_empty() {
         "".to_string();
@@ -27,22 +27,29 @@ pub fn abbreviate(phrase: &str) -> String {
         phrase
             .split_ascii_whitespace()
             .for_each(|s| {
-                if s.chars().all(|c| c.is_uppercase()) {
-                    if s.chars().any(|c| c.is_alphabetic()){
-
-                    } else {
-                        let s: char = s.chars().next().unwrap();
-                        accronym.push(s)
+                match (is_all_uppercase(s), has_punctuation(s)) {
+                    (true, true) => s
+                        .chars()
+                        .filter(|&c| c.is_alphabetic())
+                        .for_each(|c| acronym.push(c)),
+                    (true, false) => acronym.push(s.chars().next().unwrap()),
+                    (false,true) => s
+                        .split(|c: char| c.is_ascii_punctuation())
+                        .filter(|s| !s.is_empty() && s.len() > 1)
+                        .for_each(|c| {
+                            let first_char = capitalize_first_char(c).chars().next().unwrap();
+                            acronym.push(first_char)
+                        }),
+                    (_,_) => {
+                        let s = capitalize_first_char(s);
+                        s
+                            .chars()
+                            .filter(|&c| c.is_uppercase())
+                            .for_each(|c| acronym.push(c))
                     }
 
-                } else {
-                    let s = capitalize_first_char(s);
-                    s
-                        .chars()
-                        .filter(|&c| c.is_uppercase())
-                        .for_each(|c| accronym.push(c))
                 }
             })
     }
-    accronym
+    acronym
 }
