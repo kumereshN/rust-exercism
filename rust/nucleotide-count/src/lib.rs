a34babd6ee681c7905549c13a9cd93f0405533c3
N Kumeresh <kumeresh.n@s2t.ai>
2023-05-23 18:18:45 +0800

    2nd iteration of nucleotide-count

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,47 +1,18 @@
-use std::collections::{HashMap, HashSet};
-const NUCLEOTIDES: &[char;4] = &['A', 'C', 'G', 'T'];
+use std::collections::HashMap;
+const NUCLEOTIDES: &[char;4]= &['A', 'C', 'G', 'T'];
 
-fn find_invalid_char(slice: &str) -> Option<char> {
-    slice.chars().find(|c| !NUCLEOTIDES.contains(c))
-}
+
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    dna.matches()
-    match (NUCLEOTIDES.contains(&nucleotide),
-           (find_invalid_char(dna))
-    ) {
-        (true, None) => {
-            Ok(dna
-                .chars()
-                .filter(|&c| c == nucleotide)
-                .count()
-            )
-        }
-        (false, _) => {
-            Err(nucleotide)
-        }
-        (_, Some(c)) => {
-            Err(c)
-        }
-    }
+    let mut counts = nucleotide_counts(dna)?;
+    counts.remove(&nucleotide).ok_or(nucleotide)
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    let mut res: HashMap<char, usize> = NUCLEOTIDES.iter().map(|&c| (c,0)).collect();
-    let unique_chars = dna.chars().collect::<HashSet<_>>();
+    let mut map: HashMap<char, usize> = NUCLEOTIDES.iter().map(|&n| (n, 0)).collect();
 
-    for c in unique_chars.iter() {
-        match count(*c, dna) {
-            Ok(total_count) => {
-                res
-                    .entry(*c)
-                    .and_modify(|e| {*e += total_count})
-                    .or_insert(0);
-            },
-            Err(c) => {
-                return Err(c)
-            }
-        }
+    for c in dna.chars() {
+        map.get_mut(&c).map(|count| *count += 1).ok_or(c)?
     }
-    Ok(res)
+    Ok(map)
 }
