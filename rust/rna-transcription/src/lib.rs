75ea4214ca295fec55e6a5d41e36d6c941823c69
N Kumeresh <kumeresh.n@s2t.ai>
2023-06-07 16:36:59 +0800

    completed rna transcription

--- rust/rna-transcription/src/lib.rs
+++ rust/rna-transcription/src/lib.rs
@@ -1,52 +1,35 @@
-use std::collections::HashMap;
-
 #[derive(Debug, PartialEq, Eq)]
 pub struct Dna(String);
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Rna(String);
 
-const VALID_DNA_CHARS: &[char] = &['A', 'C', 'G', 'T'];
-const VALID_RNA_CHARS: &[char] = &['A', 'C', 'G', 'U'];
+const VALID_DNA_CHARS: [char; 4] = ['G', 'C', 'T', 'A'];
+const VALID_RNA_CHARS: [char; 4] = ['C', 'G', 'A', 'U'];
 
+fn validate(s: &str, chars: [char; 4]) -> Result<String, usize> {
+    match s.chars().position(|c| !chars.contains(&c)) {
+        Some(x) => Err(x),
+        None => Ok(s.to_string())
+    }
+}
+
+fn transcribe(nucleotide: char) -> char {
+    VALID_RNA_CHARS[VALID_DNA_CHARS.iter().position(|&c| c == nucleotide).unwrap()]
+}
 
 impl  Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        for (i, ch) in dna.chars().enumerate() {
-            if !VALID_DNA_CHARS.contains(&ch) {
-                return Err(i)
-            }
-        }
-        Ok(Dna(dna.parse().unwrap()))
+        validate(dna, VALID_DNA_CHARS).map(Dna)
     }
 
     pub fn into_rna(self) -> Rna {
-        let map_dna_to_rna: HashMap<char, char> = HashMap::from([
-            ('G', 'C'),
-            ('C', 'G'),
-            ('T', 'A'),
-            ('A', 'U')
-        ]);
-
-        let rna_str = self
-            .0
-            .chars()
-            .map(|c| {
-                map_dna_to_rna.get(&c).unwrap()
-            })
-            .collect::<String>();
-
-        Rna(rna_str)
+        Rna(self.0.chars().map(transcribe).collect())
     }
 }
 
 impl  Rna {
     pub fn new(rna: & str) -> Result<Rna, usize> {
-        for (i, ch) in rna.chars().enumerate() {
-            if !VALID_RNA_CHARS.contains(&ch) {
-                return Err(i)
-            }
-        }
-        Ok(Rna(rna.parse().unwrap()))
+        validate(rna, VALID_RNA_CHARS).map(Rna)
     }
 }
