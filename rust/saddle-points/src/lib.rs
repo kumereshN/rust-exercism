d4f8499fcaa3c6af3cd022853b58e8dfc91f50db
N Kumeresh <kumeresh.n@s2t.ai>
2023-06-28 13:32:58 +0800

    completed find_saddle_points

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -1,18 +1,11 @@
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
-    let mut saddle_points = Vec::new();
-    for i in 0..input.len() {
-        let row_max = input[i].iter().max().unwrap_or(&0);
-        for (j, &val) in input[i].iter().enumerate() {
-            if val == *row_max {
-                let col_min = (0..input.len())
-                    .map(|x| input[x][j])
-                    .min()
-                    .unwrap();
-                if val == col_min {
-                    saddle_points.push((i, j));
-                }
+    let mut res: Vec<(usize, usize)> = Vec::new();
+    for (r, row) in input.iter().enumerate() {
+        for (c, val) in row.iter().enumerate() {
+            if row.iter().all(|x| x <= val) && input.iter().all(|x| x[c] >= *val) {
+                res.push((r,c));
             }
         }
     }
-    saddle_points
+    res
 }
