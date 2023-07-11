32838739a0b41a81d589bc78b23ac354fed5fd3b
N Kumeresh <kumeresh.n@s2t.ai>
2023-07-11 14:22:55 +0800

    working on two buckets

--- rust/two-bucket/src/lib.rs
+++ rust/two-bucket/src/lib.rs
@@ -17,15 +17,35 @@ pub struct BucketStats {
 }
 
 pub struct BucketCapacity<'a> {
-    BucketName: &'a Bucket,
-    Capacity: u8
+    bucket_name: &'a Bucket,
+    capacity: u8,
+    water_remaining: u8,
 }
 
 impl<'a> BucketCapacity<'a> {
     fn new(bucket_name: &'a Bucket, capacity: u8) -> BucketCapacity {
         Self {
-            BucketName: bucket_name,
-            Capacity: capacity,
+            bucket_name,
+            capacity,
+            water_remaining: 0,
+        }
+    }
+
+    fn fill_bucket(bucket: &'a mut BucketCapacity, water_to_fill: u8) {
+        bucket.water_remaining += water_to_fill;
+    }
+
+    fn empty_bucket(bucket: &'a mut BucketCapacity) {
+        bucket.water_remaining = 0
+    }
+
+    fn pour_from_one_bucket_to_another(from_bucket: &'a mut BucketCapacity, to_bucket: &'a mut BucketCapacity, water_to_pour: u8) {
+        from_bucket.water_remaining = from_bucket.water_remaining.saturating_sub(to_bucket.water_remaining);
+        if to_bucket.water_remaining + water_to_pour > to_bucket.capacity {
+            let remaining_water_to_pour = to_bucket.capacity - water_to_pour;
+            to_bucket.capacity += remaining_water_to_pour;
+        } else {
+            to_bucket.water_remaining += water_to_pour;
         }
     }
 }
@@ -37,8 +57,22 @@ pub fn solve(
     goal: u8,
     start_bucket: &Bucket,
 ) -> Option<BucketStats> {
-    let bucket_1 = BucketCapacity::new(start_bucket, capacity_1);
-    unimplemented!(
-        "Given one bucket of capacity {capacity_1}, another of capacity {capacity_2}, starting with {start_bucket:?}, find pours to reach {goal}, or None if impossible"
-    );
-}
+        let mut bucket_1 = BucketCapacity::new(&Bucket::One, capacity_1);
+        let mut bucket_2 = BucketCapacity::new(&Bucket::Two, capacity_2);
+
+        BucketCapacity::fill_bucket(&mut bucket_1, 5);
+        BucketCapacity::fill_bucket(&mut bucket_2, 8);
+
+        BucketCapacity::pour_from_one_bucket_to_another(&mut bucket_2, &mut bucket_1, 2);
+        BucketCapacity::empty_bucket(&mut bucket_1);
+
+
+
+        Some(
+            BucketStats {
+                moves: 0,
+                goal_bucket: Bucket::One,
+                other_bucket: bucket_1.water_remaining
+            }
+        )
+    }
