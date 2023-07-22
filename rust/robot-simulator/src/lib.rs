12b336955aa5842c6d6551ff618b41277828dac7
kumereshN <kumeresh6@hotmail.com.com>
2023-07-22 18:11:01 +0800

    working on robot simulator

---
+++ rust/robot-simulator/src/lib.rs
@@ -0,0 +1,65 @@
+// The code below is a stub. Just enough to satisfy the compiler.
+// In order to pass the tests you can add-to or change any of this code.
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+pub struct Robot(i32,i32,Direction);
+
+impl Robot {
+    pub fn new(x: i32, y: i32, d: Direction) -> Self {
+        Self(x, y, d)
+    }
+
+    #[must_use]
+    pub fn turn_right(mut self) -> Self {
+        match self.2 {
+            Direction::North => self.2 = Direction::East,
+            Direction::East => self.2 = Direction::South,
+            Direction::South => self.2 = Direction::West,
+            Direction::West => self.2 = Direction::North
+        }
+        Self(self.0, self.1, self.2)
+    }
+
+    #[must_use]
+    pub fn turn_left(mut self) -> Self {
+        match self.2 {
+            Direction::North => self.2 = Direction::West,
+            Direction::East => self.2 = Direction::North,
+            Direction::South => self.2 = Direction::East,
+            Direction::West => self.2 = Direction::South
+        }
+        Self(self.0, self.1, self.2)
+    }
+
+    #[must_use]
+    pub fn advance(mut self) -> Self {
+        match self.2 {
+            Direction::North => self.1 += 1,
+            Direction::South => self.1 -= 1,
+            Direction::East =>  self.0 += 1,
+            Direction::West => self.0 -= 1
+        }
+
+        Self(self.0, self.1, self.2)
+    }
+
+    #[must_use]
+    pub fn instructions(self, instructions: &str) -> Self {
+        unimplemented!("Follow the given sequence of instructions: {instructions}")
+    }
+
+    pub fn position(&self) -> (i32, i32) {
+        (self.0, self.1)
+    }
+
+    pub fn direction(&self) -> &Direction {
+        &self.2
+    }
+}
