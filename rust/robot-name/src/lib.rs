9ed892b82b756c44189e44d326acf4947827c0c2
kumereshN <kumeresh6@hotmail.com.com>
2023-07-26 20:43:19 +0800

    trying out seen

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,6 +1,10 @@
+use std::collections::HashSet;
 use rand::{thread_rng};
-use rand::distributions::{Alphanumeric, Distribution, DistString, Uniform};
-pub struct Robot(String);
+use rand::distributions::{Distribution, Uniform};
+pub struct Robot{
+    robot_id: String,
+    seen: HashSet<String>
+}
 
 impl Default for Robot {
     fn default() -> Self {
@@ -8,21 +12,42 @@ impl Default for Robot {
     }
 }
 
+
 impl Robot {
+
     pub fn new() -> Self {
+       let robot = Robot::create_robot();
+
+       while robot.seen.contains(robot.robot_id) {
+           
+       }
+    }
+
+    pub fn create_robot() -> Self {
         let step = Uniform::new_inclusive(0, 9);
         let chars = Uniform::new_inclusive('A', 'Z');
+        let mut seen = HashSet::new();
+
         let mut rng = thread_rng();
+
         let chars = chars.sample_iter(&mut rng).take(2).map(|c| c.to_uppercase().to_string()).collect::<String>();
         let numbers = step.sample_iter(&mut rng).take(3).map(|n| n.to_string()).collect::<String>();
-        Self(format!("{chars}{numbers}"))
+        let robot_id = format!("{chars}{numbers}");
+
+        seen.insert(robot_id.clone());
+        Self{robot_id, seen}
     }
 
     pub fn name(&self) -> &str {
-        self.0.as_str()
+        self.robot_id.as_str()
     }
 
     pub fn reset_name(&mut self) {
-        self.0 = Robot::new().0;
+        self.robot_id = Robot::new().robot_id;
+    }
+
+    pub fn is_duplicated(&self) -> bool {
+        let seen: HashSet<String> = HashSet::new();
+        seen.contains(self.robot_id.as_str())
     }
 }
