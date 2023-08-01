eabfcbf94c7693a9ed7cfa00715289121680a6cb
kumereshN <kumeresh6@hotmail.com.com>
2023-08-01 08:11:25 +0800

    few tests in wordy

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -1,8 +1,10 @@
+#[derive(PartialEq)]
 pub enum Operations {
     Plus,
     Minus,
     Multiply,
     Divide,
+    Invalid,
     Unknown
 }
 
@@ -11,9 +13,11 @@ impl From<&str> for Operations {
         match operator {
             "plus" => Operations::Plus,
             "minus" => Operations::Minus,
-            "multiply" => Operations::Multiply,
-            "divide" => Operations::Divide,
-            _ => Operations::Unknown
+            "multiplied" => Operations::Multiply,
+            "divided" => Operations::Divide,
+            "cubed" => Operations::Unknown,
+            "exponent" => Operations::Unknown,
+            _ => Operations::Invalid
         }
     }
 }
@@ -33,11 +37,16 @@ pub fn answer(command: &str) -> Option<i32> {
         .split(|s| s == ' ' || s == '?')
         .filter_map(|s| {
             match s.into() {
-                Operations::Unknown => None,
+                Operations::Invalid => None,
                 _ => Some(s.into())
             }
         })
         .collect::<Vec<Operations>>();
+
+    if operations.iter().any(|o| *o == Operations::Unknown) ||
+        numbers.len() <= operations.len(){
+        return None
+    }
 
     let mut ops = operations.iter();
 
@@ -47,6 +56,8 @@ pub fn answer(command: &str) -> Option<i32> {
             match ops.next() {
                 Some(Operations::Plus) => { acc + x },
                 Some(Operations::Minus) => { acc - x},
+                Some(Operations::Multiply) => {acc * x},
+                Some(Operations::Divide) => {acc / x},
                 _ => acc
             }
         })
