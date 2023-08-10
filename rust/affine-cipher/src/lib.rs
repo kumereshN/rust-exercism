18617c1a1525da2673c2a4697ef53b9ae41b24bf
kumereshN <kumeresh6@hotmail.com.com>
2023-08-10 20:30:36 +0800

    completed encoding tests

--- rust/affine-cipher/src/lib.rs
+++ rust/affine-cipher/src/lib.rs
@@ -5,37 +5,57 @@ pub enum AffineCipherError {
     NotCoprime(i32),
 }
 
+fn is_coprime(mut a: i32, mut b: i32) -> bool {
+    let mut r = -1;
+
+    while r != 0 {
+        r = a % b;
+        if r == 0 {
+            break
+        }
+        (a, b) = (b, r);
+    }
+
+    b == 1
+}
+
 /// Encodes the plaintext using the affine cipher with key (`a`, `b`). Note that, rather than
 /// returning a return code, the more common convention in Rust is to return a `Result`.
 pub fn encode(plaintext: &str, a: i32, b: i32) -> Result<String, AffineCipherError> {
     let alphabets = ('a'..='z').collect::<String>();
     let alphabets_len = alphabets.len() as i32;
-    let encoded_string = plaintext
-        .chars()
-        .filter_map(|mut c| {
-            c = c.to_ascii_lowercase();
-            match (c.is_alphabetic(), c.is_numeric()) {
-                (true, false) => {
-                    let index_char = alphabets.chars().position(|x| x == c).unwrap()  as i32;
-                    let e_no = (((a * index_char) + b) %  alphabets_len) as usize;
-                    alphabets.chars().nth(e_no)
-                }
-                (false, true) => {
-                    Some(c)
-                }
-                (_, _) => None
-            }
-        })
-        .collect::<Vec<char>>()
-        .chunks(5)
-        .map(|s| {
-            format!("{} ", s.iter().collect::<String>())
-        })
-        .collect::<String>()
-        .trim_end()
-        .to_string();
-
-    Ok(encoded_string)
+    match is_coprime(a, alphabets_len) {
+        true => {
+            Ok(plaintext
+                .chars()
+                .filter_map(|mut c| {
+                    c = c.to_ascii_lowercase();
+                    match (c.is_alphabetic(), c.is_numeric()) {
+                        (true, false) => {
+                            let index_char = alphabets.chars().position(|x| x == c).unwrap()  as i32;
+                            let e_no = (((a * index_char) + b) %  alphabets_len) as usize;
+                            alphabets.chars().nth(e_no)
+                        }
+                        (false, true) => {
+                            Some(c)
+                        }
+                        (_, _) => None
+                    }
+                })
+                .collect::<Vec<char>>()
+                .chunks(5)
+                .map(|s| {
+                    format!("{} ", s.iter().collect::<String>())
+                })
+                .collect::<String>()
+                .trim_end()
+                .to_string()
+            )
+        },
+        false => {
+            Err(AffineCipherError::NotCoprime(a))
+        }
+    }
 }
 
 /// Decodes the ciphertext using the affine cipher with key (`a`, `b`). Note that, rather than
