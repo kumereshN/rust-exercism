5fa4870b63700399fba8686f0e74fc5828a9acb3
kumereshN <kumeresh6@hotmail.com.com>
2023-08-19 16:52:24 +0800

    completed diamonds

---
+++ rust/diamond/src/lib.rs
@@ -0,0 +1,43 @@
+pub fn get_diamond(c: char) -> Vec<String> {
+    if c == 'A'{
+        vec!["A".to_string()]
+    }
+    else {
+        let alphabets = ('A'..=c).collect::<Vec<char>>();
+        let reverse_alphabets = alphabets.iter().rev().copied().collect::<Vec<char>>();
+        let alphabets_len = alphabets.len();
+
+        let empty_space = "".to_string();
+        let mut odd_no_range = (1..).step_by(2);
+        let max_trailing_space = odd_no_range.nth(alphabets_len-2).unwrap();
+        let range_of_internal_whitespace=  (1..=max_trailing_space).rev().step_by(2).collect::<Vec<usize>>();
+
+        let first_half = reverse_alphabets
+            .iter()
+            .enumerate()
+            .map(|(i,&c)| {
+                match i + 1 == alphabets_len {
+                    true => {
+                        format!("{:^width$}", c, width = 2 + max_trailing_space)
+                    },
+                    false => {
+                        format!("{empty_space:>left_width$}{c:<width_1$}{c}{empty_space:<right_width$}",
+                                left_width = i,
+                                right_width = i,
+                                width_1 = range_of_internal_whitespace.get(i).unwrap() + 1
+                        )
+                    }
+                }
+            })
+            .rev();
+
+        let second_half = first_half.clone()
+            .rev()
+            .skip(1);
+
+        first_half
+            .chain(second_half)
+            .collect::<Vec<String>>()
+    }
+
+}
