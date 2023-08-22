6fac9d2f17720d1af21a7d8d38ac6d49e197a190
N Kumeresh <kumeresh.n@s2t.ai>
2023-08-22 16:37:07 +0800

    added updated solution for largest-series-products

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -4,27 +4,15 @@ pub enum Error {
     InvalidDigit(char),
 }
 pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
-    let len_of_digits = string_digits.len();
-    let has_alphabets = string_digits
+    let chs = string_digits
         .chars()
-        .filter(|c| c.is_alphabetic())
-        .map(|c| c.to_string())
-        .collect::<Vec<String>>()
-        .join("");
-    if len_of_digits < span {
-        Err(Error::SpanTooLong)
-    } else if !has_alphabets.is_empty() {
-        Err(Error::InvalidDigit(has_alphabets.chars().next().unwrap()))
+        .map(|c| c.to_digit(10).ok_or(Error::InvalidDigit(c)))
+        .collect::<Result<Vec<u32>, Error>>()?;
+    if span == 0 {
+        return Ok(1);
     }
-    else {
-        let product_of_chars = string_digits
-            .chars()
-            .collect::<Vec<char>>()
-            .windows(span)
-            .map(|c| {
-                c.iter().map(|&n| n.to_digit(10).unwrap() as u64).product::<u64>()
-            })
-            .collect::<Vec<u64>>();
-        Ok(product_of_chars.into_iter().max().unwrap())
-    }
+    chs.windows(span)
+        .map(|v| v.iter().map(|&n| n as u64).product())
+        .max()
+        .ok_or(Error::SpanTooLong)
 }
