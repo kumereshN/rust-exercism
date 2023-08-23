d54709ad66b09c51af99a135afcca5437dedd40a
N Kumeresh <kumeresh.n@s2t.ai>
2023-08-23 15:53:43 +0800

    done for From trait

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -2,55 +2,7 @@ pub struct Luhn(String);
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        let test = &self.0;
-        true
-        // unimplemented!("Determine if the current Luhn struct contains a valid credit card number.");
-    }
-}
-
-pub trait CheckType {
-    fn check_type(&self) -> String;
-}
-
-impl CheckType for &str {
-    fn check_type(&self) -> String {
-        self.chars().filter(|&c| c.is_numeric()).collect::<String>()
-    }
-}
-
-/*impl CheckType for String {
-    fn check_type(&self) -> String {
-        self.to_string()
-    }
-}*/
-
-impl CheckType for u8 {
-    fn check_type(&self) -> String {
-        self.to_string()
-    }
-}
-
-impl CheckType for u16 {
-    fn check_type(&self) -> String {
-        self.to_string()
-    }
-}
-
-impl CheckType for u32 {
-    fn check_type(&self) {
-        self.to_string();
-    }
-}
-
-impl CheckType for u64 {
-    fn check_type(&self) {
-        self.to_string();
-    }
-}
-
-impl CheckType for usize {
-    fn check_type(&self) {
-        self.to_string();
+        unimplemented!("Determine if the current Luhn struct contains a valid credit card number.");
     }
 }
 
@@ -59,9 +11,8 @@ impl CheckType for usize {
 /// by hand for the every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl<T: CheckType> From<T> for Luhn {
+impl<T> From<T> for Luhn where T: ToString {
     fn from(input: T) -> Self {
-        let convert_var = input.check_type();
-        Self(convert_var)
+        Self(input.to_string())
     }
 }
