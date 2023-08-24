7b66e3b4d65501db318d1f6745b4df3805bfa0a1
N Kumeresh <kumeresh.n@s2t.ai>
2023-08-24 20:09:38 +0800

    completed luhn-from

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -2,7 +2,42 @@ pub struct Luhn(String);
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        unimplemented!("Determine if the current Luhn struct contains a valid credit card number.");
+        if self.0.len() < 2 {
+            return false
+        }
+
+        let vec_of_nums = self
+            .0
+            .chars()
+            .rev()
+            .filter(|c| c.is_alphanumeric())
+            .enumerate()
+            .map(|(i, c)| {
+                if c.is_alphabetic() {
+                    return None
+                }
+                let digit = c.to_digit(10).unwrap();
+                match i % 2 == 0 {
+                    true => {
+                        Some(digit)
+                    },
+                    false => {
+                        let multiple = digit * 2;
+                        if multiple > 9 {
+                            Some(multiple.to_string().chars().map(|c| c.to_digit(10).unwrap()).sum::<u32>())
+                        } else {
+                            Some(multiple)
+                        }
+                    }
+                }
+            })
+            .collect::<Vec<Option<u32>>>();
+
+        if vec_of_nums.iter().all(|n| n.is_some()) {
+            vec_of_nums.iter().map(|c| c.unwrap()).sum::<u32>() % 10 == 0
+        } else {
+            false
+        }
     }
 }
 
