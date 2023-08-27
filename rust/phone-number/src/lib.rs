3927c8c67f604d765a214eb71aae90cf33045af8
kumereshN <kumeresh6@hotmail.com.com>
2023-08-27 10:18:30 +0800

    completed phone-number

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,6 +1,9 @@
 use std::iter::Peekable;
 use std::str::Chars;
 
+const PHONE_NUMBER_WITHOUT_COUNTRY_CODE: usize = 10;
+const PHONE_NUMBER_WITH_COUNTRY_CODE: usize = 11;
+
 fn is_valid_area_code(phone_no: &mut Peekable<Chars>) -> bool {
     let first_char_area_code = phone_no.peek().unwrap().to_digit(10);
     matches!(first_char_area_code, Some((2..=9)))
@@ -12,41 +15,34 @@ fn is_valid_exchange_code(phone_no: Vec<char>) -> bool {
 }
 
 pub fn number(user_number: &str) -> Option<String> {
-    let clean_user_number_vec = user_number
+    let clean_user_number = user_number
         .split(|c: char| c.is_ascii_whitespace() || c == '-' || c == '.')
         .flat_map(|s| {
             s
                 .chars()
-                .filter_map(|c| {
-                    match c.is_numeric() {
-                        true => Some(Some(c)),
-                        false => None
-                    }
-                })
-                .collect::<Vec<Option<char>>>()
+                .filter(|c| c.is_numeric())
+                .collect::<Vec<char>>()
         })
-        .collect::<Vec<Option<char>>>();
+        .collect::<String>();
 
-    let is_all_numeric_digits = clean_user_number_vec.iter().all(|c| c.is_some());
-    let clean_user_number = clean_user_number_vec.iter().flatten().collect::<String>();
-
-    match (clean_user_number.len(), is_all_numeric_digits) {
-        (10, true) => {
+    match clean_user_number.len() {
+        PHONE_NUMBER_WITHOUT_COUNTRY_CODE => {
             let mut clean_user_number_peekable = clean_user_number.chars().peekable();
-            match (is_valid_area_code(&mut clean_user_number_peekable), is_valid_exchange_code(clean_user_number.chars().collect::<Vec<char>>())) {
+            let clean_user_number_vec = clean_user_number.chars().collect::<Vec<char>>();
+            match (is_valid_area_code(&mut clean_user_number_peekable), is_valid_exchange_code(clean_user_number_vec)) {
                 (true, true) => Some(clean_user_number),
                 (_,_) => None
             }
         },
-        (11, true) => {
+        PHONE_NUMBER_WITH_COUNTRY_CODE => {
             let mut clean_user_number_iter = clean_user_number.chars();
             let country_code = clean_user_number_iter.next().unwrap();
             let mut remaining_user_number_peekable = clean_user_number_iter.clone().peekable();
             match country_code {
                 '1' => {
-                    match (is_valid_area_code(&mut remaining_user_number_peekable), is_valid_exchange_code(clean_user_number_iter.clone().collect::<Vec<char>>())) {
+                    match (is_valid_area_code(&mut remaining_user_number_peekable), is_valid_exchange_code(clean_user_number_iter.collect::<Vec<char>>())) {
                         (true, true) => {
-                            Some(clean_user_number_iter.collect::<String>())
+                            Some(remaining_user_number_peekable.collect::<String>())
                         },
                         (_,_) => None
                     }
@@ -54,7 +50,6 @@ pub fn number(user_number: &str) -> Option<String> {
                 _ => None
             }
         },
-        (_,_) => None
+        _ => None
     }
-
 }
