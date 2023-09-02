cf5481deb359575152cc2b2aa54bb234462cae13
kumereshN <kumeresh6@hotmail.com.com>
2023-09-02 16:07:59 +0800

    completed rail-fence cipher

--- rust/rail-fence-cipher/src/lib.rs
+++ rust/rail-fence-cipher/src/lib.rs
@@ -1,86 +1,39 @@
+use std::collections::HashMap;
+use unicode_segmentation::UnicodeSegmentation;
 pub struct RailFence{
-    rails: u32,
-    vec_of_chars: Vec<Vec<char>>
+    rails: u32
 }
 
 impl RailFence {
     pub fn new(rails: u32) -> RailFence {
         Self {
-            rails,
-            vec_of_chars: vec![vec!['.']; rails as usize]
+            rails
         }
     }
 
     pub fn encode(&self, text: &str) -> String {
-        let rails = self.rails;
-        let mut vec_of_chars = self.vec_of_chars
+        let graphemes: Vec<&str> = UnicodeSegmentation::graphemes(text, true).collect();
+        self.fence(graphemes.len())
             .iter()
-            .map(|c| c.repeat(text.len()))
-            .collect::<Vec<Vec<char>>>();
-
-        let first_half_iter = 0..=rails-1;
-        let second_half_iter = (0..=rails-1).rev().map(|c| c.saturating_sub(1)).take((rails.saturating_sub(2)) as usize);
-        let mut combined_iter = first_half_iter.chain(second_half_iter).cycle();
-
-        for (col_idx, c) in text.chars().enumerate() {
-            let row_idx = combined_iter.next().unwrap() as usize;
-            vec_of_chars[row_idx][col_idx] = c;
-        }
-
-        vec_of_chars
-            .iter()
-            .flat_map(|v| {
-                v.iter().filter(|c| c.is_alphanumeric())
-            })
-            .collect::<String>()
+            .map(|&i| graphemes[i])
+            .collect()
         }
 
     pub fn decode(&self, cipher: &str) -> String {
-        let rails = self.rails;
-        let mut vec_of_chars = self.vec_of_chars
-            .iter()
-            .map(|c| c.repeat(cipher.len()))
-            .collect::<Vec<Vec<char>>>();
+        let graphemes: Vec<&str> = UnicodeSegmentation::graphemes(cipher, true).collect();
+        let width = graphemes.len();
+        let indexes = self.fence(width);
+        let lookup: HashMap<_, &str> = indexes.iter().zip(graphemes).collect();
+        (0..width).map(|i| lookup[&i]).collect()
+    }
 
-        let first_half_iter = 0..=rails-1;
-        let second_half_iter = (0..=rails-1).rev().map(|c| c.saturating_sub(1)).take((rails.saturating_sub(2)) as usize);
-        let mut combined_iter = first_half_iter.chain(second_half_iter).cycle();
-
-        for col_idx in 0..cipher.len() {
-            let row_idx = combined_iter.next().unwrap() as usize;
-            vec_of_chars[row_idx][col_idx] = '*';
-        }
-
-        let mut cipher_idx_range = 0..cipher.len();
-
-        let ziz_zag_vec = vec_of_chars
-            .iter()
-            .map(|v| {
-                v
-                    .iter()
-                    .map(|&c| {
-                    if c == '*' {
-                        let cipher_idx = cipher_idx_range.next().unwrap();
-                        cipher.chars().nth(cipher_idx).unwrap()
-                    } else {
-                        '.'
-                    }
-                })
-                    .collect::<Vec<char>>()
-            })
-            .collect::<Vec<Vec<char>>>();
-
-        let mut res = String::new();
-
-        for col_idx in 0..cipher.len() {
-            for row_idx in 0..rails {
-                let ch = ziz_zag_vec[row_idx as usize][col_idx];
-                if ch != '.' {
-                    res.push(ch);
-                }
-            }
-        }
-
-        res
+    fn fence(&self, length: usize) -> Vec<usize> {
+        let mut table: Vec<(u32, usize)> = (0..self.rails)
+            .chain((1..self.rails - 1).rev())
+            .cycle()
+            .zip(0..length)
+            .collect();
+        table.sort_unstable();
+        table.iter().map(|(_, c)| *c).collect()
     }
 }
