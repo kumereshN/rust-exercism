3d6a775a254b52294601e2911a8a0711f88faa06
N Kumeresh <kumeresh.n@s2t.ai>
2023-09-07 17:35:54 +0800

    shorten rotation-cipher

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,20 +1,12 @@
 use std::collections::HashMap;
 
-const LARGEST_ALPHABET_INDEX: i8 = 25;
+const LARGEST_ALPHABET_INDEX: i8 = 26;
 
 fn get_transpose_char(c: char, key: i8) -> char {
     let alphabets_hmap: HashMap<char, usize> = ('a'..='z').enumerate().map(|(i, c)| (c, i)).collect();
     let char_idx = *alphabets_hmap.get(&c).unwrap() as i8;
-    let mut transpose_idx = char_idx + key;
-
-    if transpose_idx > LARGEST_ALPHABET_INDEX {
-        transpose_idx = transpose_idx % LARGEST_ALPHABET_INDEX - 1;
-    }
-    if transpose_idx.is_negative() {
-        transpose_idx = transpose_idx.rem_euclid(LARGEST_ALPHABET_INDEX) + 1;
-    }
-
-
+    let transpose_idx = (char_idx + key).rem_euclid(LARGEST_ALPHABET_INDEX);
+    
     *alphabets_hmap
         .iter()
         .find(|(&_c, &i)| {
@@ -25,7 +17,6 @@ fn get_transpose_char(c: char, key: i8) -> char {
 }
 
 pub fn rotate(input: &str, key: i8) -> String {
-
     input
         .chars()
         .map(|c| {
