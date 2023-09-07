e1d6fe43c631617ce70397e97f3442501e37ac3d
N Kumeresh <kumeresh.n@s2t.ai>
2023-09-07 15:37:35 +0800

    working on rotational-cipher

---
+++ rust/rotational-cipher/src/lib.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+
+const LARGEST_ALPHABET_INDEX: i8 = 25;
+
+fn get_transpose_char(c: char, key: i8) -> char {
+    let alphabets_hmap: HashMap<char, usize> = ('a'..='z').enumerate().map(|(i, c)| (c, i)).collect();
+    let char_idx = *alphabets_hmap.get(&c).unwrap() as i8;
+    let mut transpose_idx = char_idx + key;
+
+    match transpose_idx {
+        0..=LARGEST_ALPHABET_INDEX => {
+            *alphabets_hmap
+                .iter()
+                .find(|(&_c, &i)| {
+                    i == transpose_idx as usize
+                })
+                .unwrap()
+                .0
+        }
+        _ => {
+            transpose_idx = transpose_idx % LARGEST_ALPHABET_INDEX - 1;
+            *alphabets_hmap
+                .iter()
+                .find(|(&_c, &i)| {
+                    i == transpose_idx as usize
+                })
+                .unwrap()
+                .0
+        }
+    }
+}
+
+pub fn rotate(input: &str, key: i8) -> String {
+
+    input
+        .chars()
+        .map(|c| {
+            get_transpose_char(c, key)
+        })
+        .collect::<String>()
+    }
