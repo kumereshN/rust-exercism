77076bf3796a7fbf1705d93c131dd59db2bac828
N Kumeresh <kumeresh.n@s2t.ai>
2023-09-12 15:47:41 +0800

    left with 2 tests on word-count

---
+++ rust/word-count/src/lib.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+
+/// Count occurrences of words.
+pub fn word_count(words: &str) -> HashMap<String, u32> {
+    words
+        .split(&[' ', ',', '\n'])
+        .filter_map(|word| {
+            let filtered_word = word.chars().filter(|&c| c == '\'' || c.is_alphanumeric()).collect::<String>();
+            match filtered_word.is_empty() {
+                true => None,
+                false => Some(filtered_word.to_ascii_lowercase())
+            }
+        })
+        .fold(HashMap::new(), |mut acc, word| {
+            acc
+                .entry(word.to_string())
+                .and_modify(|counter| *counter += 1)
+                .or_insert(1);
+            acc
+        })
+
+    // unimplemented!("Count of occurrences of words in {words:?}");
+}
