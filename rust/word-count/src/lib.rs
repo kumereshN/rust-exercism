0f5ce12290dc6e7f185351f07a03590deedcb77c
N Kumeresh <kumeresh.n@s2t.ai>
2023-09-12 16:07:20 +0800

    completed word-count

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,5 +1,17 @@
 use std::collections::HashMap;
 
+fn remove_quotations_in_string(word: String) -> String {
+    let vec_char = word.chars().collect::<Vec<char>>();
+    let first_char = *vec_char.first().unwrap();
+    let last_char = *vec_char.last().unwrap();
+
+    if first_char == '\'' && last_char == '\'' {
+        vec_char.iter().filter(|&&c| c.is_alphanumeric()).collect::<String>()
+    } else {
+        word
+    }
+}
+
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
     words
@@ -8,7 +20,7 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
             let filtered_word = word.chars().filter(|&c| c == '\'' || c.is_alphanumeric()).collect::<String>();
             match filtered_word.is_empty() {
                 true => None,
-                false => Some(filtered_word.to_ascii_lowercase())
+                false => Some(remove_quotations_in_string(filtered_word).to_ascii_lowercase())
             }
         })
         .fold(HashMap::new(), |mut acc, word| {
@@ -18,6 +30,4 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
                 .or_insert(1);
             acc
         })
-
-    // unimplemented!("Count of occurrences of words in {words:?}");
 }
