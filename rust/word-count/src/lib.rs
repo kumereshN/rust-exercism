397b1208b610b63e5e51a0f3bd2cb93426bdd224
N Kumeresh <kumeresh.n@s2t.ai>
2023-09-12 16:10:52 +0800

    completed word-count

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-fn remove_quotations_in_string(word: String) -> String {
+fn check_and_remove_quotations_in_string(word: String) -> String {
     let vec_char = word.chars().collect::<Vec<char>>();
     let first_char = *vec_char.first().unwrap();
     let last_char = *vec_char.last().unwrap();
@@ -20,7 +20,7 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
             let filtered_word = word.chars().filter(|&c| c == '\'' || c.is_alphanumeric()).collect::<String>();
             match filtered_word.is_empty() {
                 true => None,
-                false => Some(remove_quotations_in_string(filtered_word).to_ascii_lowercase())
+                false => Some(check_and_remove_quotations_in_string(filtered_word).to_ascii_lowercase())
             }
         })
         .fold(HashMap::new(), |mut acc, word| {
