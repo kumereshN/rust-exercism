df8d2ecfb72b544c1673bd54827aae932d5da185
kumereshN <kumeresh6@hotmail.com.com>
2023-09-21 21:53:15 +0800

    book-store

--- rust/book-store/src/lib.rs
+++ rust/book-store/src/lib.rs
@@ -1,23 +1,21 @@
 use std::collections::HashSet;
-use std::ops::Div;
-use std::process::Output;
 
 const BOOK_PRICE: u32 = 800;
 
 enum BookDiscount {
-    DiscountOfTwo = 95,
-    DiscountOfThree = 90,
-    DiscountOfFour = 80,
-    DiscountOfFive = 75
+    Two = 95,
+    Three = 90,
+    Four = 80,
+    Five = 75
 }
 
 impl BookDiscount {
     pub fn get_discount(self) -> f32 {
         match self {
-            BookDiscount::DiscountOfTwo => 0.95,
-            BookDiscount::DiscountOfThree => 0.90,
-            BookDiscount::DiscountOfFour => 0.80,
-            BookDiscount::DiscountOfFive => 0.75
+            BookDiscount::Two => 0.95,
+            BookDiscount::Three => 0.90,
+            BookDiscount::Four => 0.80,
+            BookDiscount::Five => 0.75
         }
     }
 }
@@ -33,10 +31,10 @@ pub fn lowest_price(books: &[u32]) -> u32 {
             let total_price_of_books = (total_books_in_basket as u32 * BOOK_PRICE) as f32;
             if books_basket_hashset.len() == total_books_in_basket {
                 match total_books_in_basket {
-                    2 => (BookDiscount::DiscountOfTwo.get_discount() * (total_price_of_books)) as u32,
-                    3 => (BookDiscount::DiscountOfThree.get_discount() * (total_price_of_books)) as u32,
-                    4 => (BookDiscount::DiscountOfFour.get_discount() * (total_price_of_books)) as u32,
-                    5 => (BookDiscount::DiscountOfFive.get_discount() * (total_price_of_books)) as u32,
+                    2 => (BookDiscount::Two.get_discount() * (total_price_of_books)) as u32,
+                    3 => (BookDiscount::Three.get_discount() * (total_price_of_books)) as u32,
+                    4 => (BookDiscount::Four.get_discount() * (total_price_of_books)) as u32,
+                    5 => (BookDiscount::Five.get_discount() * (total_price_of_books)) as u32,
                     _ => panic!("Something went wrong")
 
                 }
