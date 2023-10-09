8d2d652d76e8ce6d8f0c5975f82330e413f2e410
N Kumeresh <kumeresh.n@s2t.ai>
2023-10-09 16:00:18 +0800

    completed decimals

--- rust/decimal/src/lib.rs
+++ rust/decimal/src/lib.rs
@@ -1,31 +1,95 @@
+use num_bigint::BigInt;
+use std::cmp::Ordering;
+use std::ops::{Add, Mul, Sub};
 
 /// Type implementing arbitrary-precision decimal arithmetic
-#[derive(PartialOrd, PartialEq)]
+#[derive(Debug, Clone, Eq)]
 pub struct Decimal{
-    decimal: f64
+    int: BigInt,
+    shift: usize,
     // implement your type here
 }
 
 impl Decimal {
     pub fn try_from(input: &str) -> Option<Decimal>
     {
-        match input.parse::<f64>() {
-            Ok(decimal) => Some(Decimal{ decimal }),
-            Err(_) => None
+        let mut int = String::new();
+        let mut shift = 0;
+        for (i, c) in input.chars().enumerate() {
+            if c == '.' {
+                shift = input.len() - 1 - i;
+            } else {
+                int.push(c);
+            }
         }
+        let int = BigInt::parse_bytes(int.as_bytes(), 10)?;
+        Some(Self { int, shift})
         // todo!("Create a new decimal with a value of {input}")
     }
-}
 
-// Implement Eq and PartialEq for Decimal
-impl Eq for Decimal {}
+    fn upshift(&self, shift: usize) -> Self {
+        if self.shift >= shift {
+            panic!("invalid upshift");
+        }
+        let shift_delta = shift - self.shift;
+        Self {
+            int: self.int.clone() * BigInt::from(10).pow(shift_delta as u32),
+            shift,
+        }
+    }
 
-impl PartialEq<Decimal> for f64 {
-    fn eq(&self, other: &Decimal) -> bool {
-        let res = self - other.decimal;
-        res == 0.0
-        // *self == other.decimal
+    fn normalize_shifts(lhs: Self, rhs: Self) -> (Self, Self) {
+        let left_shift = lhs.shift;
+        let right_shift = rhs.shift;
+        match lhs.shift.cmp(&rhs.shift) {
+            Ordering::Equal => (lhs, rhs),
+            Ordering::Less => (lhs.upshift(right_shift), rhs),
+            Ordering::Greater => (lhs, rhs.upshift(left_shift))
+        }
     }
 }
 
-// See https://exercism.org/tracks/rust/exercises/decimal/solutions/vuongkienthanh
+impl Add for Decimal {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        let (mut lhs, rhs) = Self::normalize_shifts(self, rhs);
+        lhs.int += rhs.int;
+        lhs
+    }
+}
+
+impl Sub for Decimal {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        let (mut lhs, rhs) = Self::normalize_shifts(self, rhs);
+        lhs.int -= rhs.int;
+        lhs
+    }
+}
+
+impl Mul for Decimal {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        let (mut lhs, rhs) = Self::normalize_shifts(self, rhs);
+        lhs.int *= rhs.int;
+        lhs.shift += rhs.shift;
+        lhs
+    }
+}
+
+impl PartialEq for Decimal {
+    fn eq(&self, other: &Self) -> bool {
+        let (lhs, rhs) = Self::normalize_shifts(self.clone(), other.clone());
+        lhs.shift == rhs.shift && lhs.int == rhs.int
+    }
+}
+
+
+impl PartialOrd for Decimal {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let (lhs, rhs) = Self::normalize_shifts(self.clone(), other.clone());
+        lhs.int.partial_cmp(&rhs.int)
+    }
+}
+
+// See https://exercism.org/tracks/rust/eNpxercises/decimal/solutions/vuongkienthanh
