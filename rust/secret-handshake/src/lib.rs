5873273938ba50a2b74764e0f96b7d99813a3268
N Kumeresh <kumeresh.n@s2t.ai>
2023-10-26 17:37:24 +0800

    completed secret-handshake

--- rust/secret-handshake/src/lib.rs
+++ rust/secret-handshake/src/lib.rs
@@ -1,16 +1,28 @@
 pub fn actions(n: u8) -> Vec<&'static str> {
-    let mut res: Vec<&'static str> = vec![];
-    let binary_number = format!("{:b}", 31);
-    match binary_number.as_str() {
-        "1" => res.push("wink"),
-        _ => panic!("Something went wrong")
-    }
-    (0..8)
+    let mut res = (0..8)
         .rev()
-        .map(|i| {
-            if (n & (1 << i)) != 0 {
-
+        .filter_map(|i| {
+            let bit_wise_ops = n & (1 << i);
+            if bit_wise_ops != 0 {
+                match i {
+                    0 => Some("wink"),
+                    1 => Some("double blink"),
+                    2 => Some("close your eyes"),
+                    3 => Some("jump"),
+                    4 => Some("reverse"),
+                    _ => None
+                }
+            } else {
+                None
             }
         })
-    res
+        .rev()
+        .collect::<Vec<&'static str>>();
+
+    if res.contains(&"reverse"){
+        res.pop();
+        res.into_iter().rev().collect::<Vec<&'static str>>()
+    } else {
+        res
+    }
 }
