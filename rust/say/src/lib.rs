386f030c1891c4d64ae2a713dfe37d6537e7a750
kumereshN <kumeresh6@hotmail.com.com>
2023-11-11 11:58:12 +0800

    added to_word fn

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -1,11 +1,55 @@
 use std::collections::HashMap;
 
+pub fn to_word(concat_n: String,
+               ones_number_map: &HashMap<char, &str>,
+               tens_number_map: &HashMap<&str, &str>,
+               twenty_to_ninety_nine_map: &HashMap<&str, &str>) -> String {
+    let len_of_number =concat_n.len();
+    let first_char = concat_n.chars().next().unwrap();
+    let last_char = concat_n.chars().last().unwrap();
+
+    match len_of_number {
+        1 => {
+            ones_number_map.get(&first_char).unwrap().to_string()
+        },
+        2 => {
+            match first_char {
+                '1' => {
+                    tens_number_map.get(concat_n.as_str()).unwrap().to_string()
+                },
+                '2'..='9' => {
+                    if last_char == '0' {
+                        twenty_to_ninety_nine_map.get(concat_n.as_str()).unwrap().to_string()
+                    } else {
+                        concat_n
+                            .chars()
+                            .enumerate()
+                            .fold(String::new(), |mut acc, (i,c) | {
+                                if i == 0 {
+                                    let first_str = *twenty_to_ninety_nine_map.get(format!("{}0",c).as_str()).unwrap();
+                                    acc.push_str(format!("{}-", first_str).as_str())
+                                } else {
+                                    let last_str = *ones_number_map.get(&c).unwrap();
+                                    acc.push_str(last_str)
+                                }
+                                acc
+                            })
+                    }
+
+                },
+                _ => {
+                    panic!("Error")
+                }
+            }
+        }
+        3 => {
+            format!("{} hundred", ones_number_map.get(&first_char).unwrap())
+        }
+        _ => panic!("Something went wrong")
+    }
+}
+
 pub fn encode(n: u64) -> String {
-    let len_of_number = n.to_string().len();
-    let vec_char = n
-        .to_string()
-        .chars()
-        .collect::<Vec<char>>();
 
     let ones_number_map: HashMap<char, &str> = HashMap::from([
         ('0', "zero"),
@@ -44,47 +88,13 @@ pub fn encode(n: u64) -> String {
         ("90", "ninety")
     ]);
 
-    let concat_n = vec_char.iter().map(|&c|c.to_string()).collect::<Vec<String>>().join("");
-    let first_char = concat_n.chars().next().unwrap();
-    let last_char = concat_n.chars().last().unwrap();
+    let concat_n = n
+        .to_string()
+        .chars()
+        .map(|c| c.to_string())
+        .collect::<Vec<String>>()
+        .join("");
 
-    match len_of_number {
-        1 => {
-            ones_number_map.get(&first_char).unwrap().to_string()
-        },
-        2 => {
-            match first_char {
-                '1' => {
-                    tens_number_map.get(concat_n.as_str()).unwrap().to_string()
-                },
-                '2'..='9' => {
-                    if last_char == '0' {
-                        twenty_to_ninety_nine_map.get(concat_n.as_str()).unwrap().to_string()
-                    } else {
-                        concat_n
-                            .chars()
-                            .enumerate()
-                            .fold(String::new(), |mut acc, (i,c) | {
-                                if i == 0 {
-                                    let first_str = *twenty_to_ninety_nine_map.get(format!("{}0",c).as_str()).unwrap();
-                                    acc.push_str(format!("{}-", first_str).as_str())
-                                } else {
-                                    let last_str = *ones_number_map.get(&c).unwrap();
-                                    acc.push_str(last_str)
-                                }
-                                acc
-                        })
-                    }
+    to_word(concat_n, &ones_number_map, &tens_number_map, &twenty_to_ninety_nine_map)
 
-                },
-                _ => {
-                    panic!("Error")
-                }
-            }
-        }
-        3 => {
-            format!("{} hundred", ones_number_map.get(&first_char).unwrap())
-        }
-        _ => panic!("Something went wrong")
-    }
 }
