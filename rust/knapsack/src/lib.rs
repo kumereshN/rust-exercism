f4f348f319fb6ef9fa569d0315988679e8d386f0
kumereshN <kumeresh6@hotmail.com.com>
2023-11-19 13:03:29 +0800

    completed knapsack

--- rust/knapsack/src/lib.rs
+++ rust/knapsack/src/lib.rs
@@ -1,27 +1,23 @@
-use std::cmp::max;
 
+#[derive(Clone)]
 pub struct Item {
     pub weight: u32,
     pub value: u32,
 }
 
-pub fn knap_sack(max_w:u32, wt: Vec<u32>, val: Vec<u32>, n: usize) -> u32 {
-    let mut dp = vec![0u32; max_w as usize];
-
-    for i in 1..=n {
-        for w in (1..=max_w).rev() {
-            if wt[i-1] <= w {
-                dp[w as usize - 1] = max(dp[w as usize - 1], dp[(w as usize - wt[i-1] as usize).saturating_sub(1)] + val[i-1])
-            }
-        }
-    }
-    dp[max_w as usize - 1]
-}
 
 pub fn maximum_value(_max_weight: u32, _items: &[Item]) -> u32 {
-    let weight = _items.iter().map(|i| i.weight).collect::<Vec<u32>>();
-    let profit = _items.iter().map(|i| i.value).collect::<Vec<u32>>();
-    let n = profit.len();
+    let mut items = _items.to_vec();
+    let mut max_val = 0;
 
-    knap_sack(_max_weight, weight, profit, n)
+    while let Some(item) = items.pop() {
+        let val = if item.weight <= _max_weight {
+            item.value + maximum_value(_max_weight - item.weight, &items)
+        } else {
+            0
+        };
+
+        max_val = if val > max_val { val } else { max_val }
+    }
+    max_val
 }
