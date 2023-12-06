73e1904846e094edeb7330c6cef5eab2f28d12d6
kumereshN <kumeresh6@hotmail.com.com>
2023-12-06 20:35:51 +0800

    working on poker

--- rust/poker/src/lib.rs
+++ rust/poker/src/lib.rs
@@ -1,7 +1,25 @@
+use std::collections::HashSet;
+
 /// Given a list of poker hands, return a list of those hands which win.
 ///
 /// Note the type signature: this function should return _the same_ reference to
 /// the winning hand(s) as were passed in, not reconstructed strings which happen to be equal.
+
+
+
 pub fn winning_hands<'a>(hands: &[&'a str]) -> Vec<&'a str> {
-    todo!("Out of {hands:?}, which hand wins?")
+
+    let hset: HashSet<_> = hands
+        .iter()
+        .fold(HashSet::new(), |mut acc, x| {
+            acc.insert(x);
+            acc
+        });
+
+    if hset.len() == 1 {
+        hands.to_vec()
+    } else {
+        hands.to_vec()
+    }
+    // todo!("Out of {hands:?}, which hand wins?")
 }
