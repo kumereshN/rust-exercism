b85f9588ffbf73af51d1bb8a44c7a17c0f9bf87f
KumereshN <kumeresh6@hotmail.com>
2024-03-31 15:27:01 +0800

    completed matrix

--- rust/matrix/src/lib.rs
+++ rust/matrix/src/lib.rs
@@ -1,49 +1,41 @@
 pub struct Matrix {
-    rows: Vec<Vec<u32>>,
-    columns: Vec<Vec<u32>>
+    data: Vec<Vec<u32>>
     // Implement your Matrix struct
 }
 
 impl Matrix {
+    const PARSE_ERROR: &'static str = "Unable to parse character";
     pub fn new(input: &str) -> Self {
-        let rows = input
+        let data = input
             .lines()
             .map(|c| c
                 .split_whitespace()
                 .map(|n| {
-                    n.parse::<u32>().unwrap()
+                    n.parse::<u32>().expect(Self::PARSE_ERROR)
                 })
                 .collect::<Vec<u32>>()
             )
             .collect::<Vec<Vec<u32>>>();
         
-        let len_of_matrix = rows.first().unwrap().len();
-        let columns = (0..len_of_matrix)
-            .map(|i| {
-                rows
-                    .iter()
-                    .map(|v| {
-                        *v.get(i).unwrap()
-                    })
-                    .collect::<Vec<u32>>()
-            })
-            .collect::<Vec<_>>();
-        
         Self {
-            rows,
-            columns
+            data
         }
 
         // todo!("Create new method to store the {input}")
     }
 
     pub fn row(&self, row_no: usize) -> Option<Vec<u32>> {
-        self.rows.get(row_no-1).cloned()
+        self.data
+            .get(row_no-1)
+            .cloned()
         // todo!("Return the row at {row_no} (1-indexed) or None if the number is invalid")
     }
 
     pub fn column(&self, col_no: usize) -> Option<Vec<u32>> {
-        self.columns.get(col_no-1).cloned()
+        self.data
+            .iter()
+            .map(|row| row.get(col_no-1).cloned())
+            .collect()
         // todo!("Return the column at {col_no} (1-indexed) or None if the number is invalid")
     }
 }
